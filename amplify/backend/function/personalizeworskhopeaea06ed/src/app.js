46d6f0846e01d7e6e16134297af4af7eef8fe833
perima@amazon.com <perima@amazon.com>
2019-06-27 14:13:05 +0000

    personalize calls now work!

---
+++ amplify/backend/function/personalizeworskhopeaea06ed/src/app.js
@@ -0,0 +1,126 @@
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+
+Amplify Params - DO NOT EDIT */
+var AWS = require('aws-sdk');
+var express = require('express')
+var bodyParser = require('body-parser')
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+var app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
+
+
+/**********************
+ * Example get method *
+ **********************/
+
+app.get('/items', function(req, res) {
+  // Add your code here
+ // res.json({success: 'get call succeed!', url: req.url});
+  
+  var personalizeruntime = new AWS.PersonalizeRuntime({apiVersion: '2018-05-22'}); 
+           var params = {
+      campaignArn: 'arn:aws:personalize:us-west-2:927378468065:campaign/personalize-workshop', /* required */
+
+      numResults: '20',
+      userId: '5'
+    };
+    personalizeruntime.getRecommendations(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+         res.json({success: 'get call failed!', url: req.url, err: err, errstack: err.stack});
+      }
+        
+      else  {
+         console.log(data);           // successful response
+          res.json({success: 'get call succeed (2)!', url: req.url, data: data});
+      }   
+       
+    });
+  
+ 
+  
+  
+});
+
+app.get('/items/*', function(req, res) {
+
+
+
+  // Add your code here
+    
+    
+  
+});
+
+/****************************
+* Example post method *
+****************************/
+
+app.post('/items', function(req, res) {
+  // Add your code here
+  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+});
+
+app.post('/items/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+});
+
+/****************************
+* Example put method *
+****************************/
+
+app.put('/items', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+app.put('/items/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+/****************************
+* Example delete method *
+****************************/
+
+app.delete('/items', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.delete('/items/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
 import React from 'react';
 import logo from './AWS_logo_RGB_REV.png';
-//import './App.css';
+import './personalize.css';
 
-function App() {
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+import Amplify, {API} from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  dense: {
+    marginTop: theme.spacing(2),
+  },
+  menu: {
+    width: 200,
+  },
+}));
+
+async function getData() { 
+    let apiName = 'personalize';
+    let path = '/items/?userId=5';
+    let myInit = { // OPTIONAL
+        headers: {} // OPTIONAL
+    }
+    return await API.get(apiName, path, myInit);
+}
+
+ function App() {
+  
+  
+  
+  const constdata =   getData().then(response => {
+      console.log('constdata response', response);
+  });
+  
+  console.log('constdata', constdata);
+  
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    name: 'Cat in the Hat',
+    age: '',
+    multiline: 'Controlled',
+    currency: 'EUR',
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+
   return (
     <div className="App">
      
         <img src={logo} alt="logo" />
         <p>
           <code> Amazon Personalize Workshop</code>
+          <p>
+          <TextField
+              id="outlined-name"
+              label="User-id"
+              className={classes.textField}
+              value={values.name}
+              onChange={handleChange('name')}
+              margin="normal"
+              variant="outlined"
+            />
+            </p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Amazon Personalize Workshop
-        </a>
      
     </div>
   );
